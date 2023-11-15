9f3a2fe281f7eddaf910b4f7f76f628af4278153
samcab28 <samircabrera2528@gmail.com>
2023-11-14 22:36:53 -0600

    avance

--- Ingeniero/src/main/java/moding/ingeniero/IngenieroApplication.java
+++ Ingeniero/src/main/java/moding/ingeniero/IngenieroApplication.java
@@ -46,33 +46,6 @@ public class IngenieroApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
-        contactoRepository.save(new Contacto("Juan", "Pérez", "12345678", "juan@gmail.com", "@juan_telegram"));
-        contactoRepository.save(new Contacto("María", "López", "23456789", "maria@gmail.com", "@maria_telegram"));
-        contactoRepository.save(new Contacto("Ana", "Gómez", "34567890", "ana@gmail.com", "@ana_telegram"));
-        contactoRepository.save(new Contacto("Carlos", "Rodríguez", "45678901", "carlos@gmail.com", "@carlos_telegram"));
-        contactoRepository.save(new Contacto("Laura", "Fernández", "56789012", "laura@gmail.com", "@laura_telegram"));
-
-        // Contacto 6-10
-        contactoRepository.save(new Contacto("Pedro", "Martínez", "67890123", "pedro@gmail.com", "@pedro_telegram"));
-        contactoRepository.save(new Contacto("Isabel", "Hernández", "78901234", "isabel@gmail.com", "@isabel_telegram"));
-        contactoRepository.save(new Contacto("Miguel", "González", "89012345", "miguel@gmail.com", "@miguel_telegram"));
-        contactoRepository.save(new Contacto("Sofía", "Díaz", "90123456", "sofia@gmail.com", "@sofia_telegram"));
-        contactoRepository.save(new Contacto("Antonio", "Suárez", "01234567", "antonio@gmail.com", "@antonio_telegram"));
-
-        // Contacto 11-15
-        contactoRepository.save(new Contacto("Elena", "Ramírez", "98765432", "elena@gmail.com", "@elena_telegram"));
-        contactoRepository.save(new Contacto("Javier", "Pérez", "87654321", "javier@gmail.com", "@javier_telegram"));
-        contactoRepository.save(new Contacto("Carmen", "Gutiérrez", "76543210", "carmen@gmail.com", "@carmen_telegram"));
-        contactoRepository.save(new Contacto("Hugo", "Ortega", "65432109", "hugo@gmail.com", "@hugo_telegram"));
-        contactoRepository.save(new Contacto("Luisa", "Morales", "54321098", "luisa@gmail.com", "@luisa_telegram"));
-
-        // Contacto 16-20
-        contactoRepository.save(new Contacto("Fernando", "García", "43210987", "fernando@gmail.com", "@fernando_telegram"));
-        contactoRepository.save(new Contacto("Beatriz", "Mendoza", "32109876", "beatriz@gmail.com", "@beatriz_telegram"));
-        contactoRepository.save(new Contacto("Diego", "Cruz", "21098765", "diego@gmail.com", "@diego_telegram"));
-        contactoRepository.save(new Contacto("Lucía", "Silva", "10987654", "lucia@gmail.com", "@lucia_telegram"));
-        contactoRepository.save(new Contacto("Ricardo", "Reyes", "09876543", "ricardo@gmail.com", "@ricardo_telegram"));
-
     }
 
 
--- Ingeniero/src/main/java/moding/ingeniero/controladorLogin/LoginController.java
+++ Ingeniero/src/main/java/moding/ingeniero/controladorLogin/LoginController.java
@@ -28,22 +28,22 @@ public class LoginController {
     @ResponseBody
     public String procesarLogin(@RequestParam String username, @RequestParam String password) {
         List<Ingeniero> ingenieros = ingenieroRepository.findByCorreoAndPassword(username, password);
+        System.out.println("username agarrado: " + username + " password agarrado: " + password);
         for(Ingeniero ingenieros1: ingenieroRepository.findAll()){
             System.out.println("nombre: "+ingenieros1.getNombre()+"  correo " + ingenieros1.getCorreo() + " y contrasena: "+ingenieros1.getPassword());
 
-            if(username == ingenieros1.getCorreo() & password == ingenieros1.getPassword()){
+            if(username.equals(ingenieros1.getCorreo()) && password.equals(ingenieros1.getPassword())){
                 System.out.println("login exitoso con el correo: " + ingenieros1.getCorreo() );
                 return "login exitoso";
             }
         }
         System.out.println("login no exitoso");
-        return "login exitoso";
+        return "login no exitoso";
     }
 
 
     @RequestMapping("/registro")
     public String registro(Model model) {
-        // Agrega un nuevo objeto Ingeniero al modelo para ser utilizado en el formulario de registro
         model.addAttribute("nuevoIngeniero", new Ingeniero());
         return "registro";
     }

55f6593184e803d1153038039d60db235075686e
Ben Farz <benfarz.bf25@gmail.com>
2023-11-22 23:20:10 -0600

    "Agricultor Debug"

--- Agricultor/src/main/java/modagri/agricultor/conexion/Controller.java
+++ Agricultor/src/main/java/modagri/agricultor/conexion/Controller.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Vector;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
